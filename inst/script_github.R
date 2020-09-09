40995f512d582e81daecb690110e3ba60785dbcc
fndemarqui <fndemarqui@gmail.com>
2020-09-09 14:07:04 -0300

    adicao do banco capacidade

--- inst/script_github.R
+++ inst/script_github.R
@@ -1,10 +1,14 @@
 
 
+git config --global user.email "fndemarqui@gmail.com"
+git config --global user.name "fndemarqui"
+
+
 # para adicionar tudo
 git add .
 
 # para comitar
-git commit -m "atualizacao da documentacao"
+git commit -m "adicao do banco capacidade"
 
 # para subir:
 git push
